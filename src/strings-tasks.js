566c40a7b79a5538dd9c71944675281b816671aa
AlbinaT <telemesova96@gmail.com>
2025-09-17 17:40:27 +0500

    feat: add removeFirstOccurrences function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -152,8 +152,13 @@ function repeatString(str, times) {
  *   removeFirstOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeFirstOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeFirstOccurrences(str, value) {
+  const index = str.indexOf(value);
+  if (index >= 0) {
+    const valueLength = value.length;
+    return str.slice(0, index) + str.slice(index + valueLength);
+  }
+  return str;
 }
 
 /**
